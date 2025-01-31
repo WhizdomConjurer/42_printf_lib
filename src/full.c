fdf84415c4df00bf65168e4a86e6aaebf8f67527
Rene <riebschlaeger1998@gmail.com>
2025-01-31 17:07:28 +0100

    parsing flags until space

--- src/full.c
+++ src/full.c
@@ -6,7 +6,7 @@
 /*   By: whi <whi@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/31 12:36:39 by whi               #+#    #+#             */
-/*   Updated: 2025/01/31 16:27:20 by whi              ###   ########.fr       */
+/*   Updated: 2025/01/31 17:03:14 by whi              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,37 @@ void	ft_int_flags(t_count main_data)
 	main_data.conversion = 0;
 }
 
+int	ft_isflag(char c)
+{
+	if (c == '-' || c == '0' || c == '.' || c == '*'
+		|| c == '#' || c == ' ' || c == '+')
+		return (1);
+	return (0);
+}
+
+int	ft_isconversion(char c)
+{
+	if (c == 'c' || c == 's' || c == 'd' || c == 'i' || c == 'u'
+		|| c == 'x' || c == 'X' || c == 'p' || c == '%')
+		return (1);
+	return (0);
+}
+
+int	ft_is_flag(char c)
+{
+	return (ft_isdigit(c) || ft_isflag(c) || ft_istconversion(c));
+}
+
+void	ft_parse_flags(t_count main_data, int x)
+{
+	while (main_data.str[x++] && ft_is_flag(main_data.str[x]))
+	{
+		if (main_data.str[x] == "-")
+			ft_left_flag(main_data);
+	}
+	
+}
+
 int	ft_printf(const char *str, ...)
 {
 	int		x;
@@ -33,11 +64,12 @@ int	ft_printf(const char *str, ...)
 	main_data.count = 0;
 	va_start(main_data.args, str);
 	main_data.str = str;
+	ft_int_flags(main_data);
 	while (str[x])
 	{
-		if (str[x] == '%')
+		if (str[x] == '%' && str[x + 1] != '\0')
 		{
-			ft_prozess(main_data, x + 1);
+			ft_parse_flags(main_data, x + 1);
 			x++;
 		}
 		else
